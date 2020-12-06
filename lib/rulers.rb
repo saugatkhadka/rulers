ef921d9857ecf4995fcf67ad7d7b8afbd5083d7b
Saugat Khadka <ksaugat@gmail.com>
2020-12-06 22:59:11 +0545

    Add html return on app call

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,12 @@
 require "rulers/version"
 
 module Rulers
+	class Application
+		def call(env)
+			[200, {'Content-Type' => 'text/html'}, ['Hello World!']]
+		end
+	end
+
   class Error < StandardError; end
   # Your code goes here...
 end
